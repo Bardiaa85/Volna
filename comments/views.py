f595d91cebb929e3bb7987dec16ba8447d9796d1
Bardiaa85 <bardiacodes85@gmail.com>
2024-12-22 18:26:30 +0330

    Dynamic commenting was added

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+import json
+from django.shortcuts import render , redirect
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from .models import Comment
 from releases.models import Release
 from news.models import Article
@@ -7,30 +9,35 @@ def adding_comment(request , slug):
     if request.method == "POST":
         release = Release.objects.filter(slug = slug)
         article = Article.objects.filter(slug = slug)
-        comment_content = request.POST.get("comment")
+        json_data = json.loads(request.body)
+        comment_content = json_data.get("content" , "")
         if len(article) == 0 :
             release = Release.objects.get(slug = slug)
             new_comment = Comment(content = comment_content , related_release = release , related_user = request.user)
             if new_comment.content == "" :       
-                return redirect("releases:single_release_page" , slug = new_comment.related_release.slug)
+                return JsonResponse({"has_content" : False , "status" : "success" , "message" : "Data saved successfully"})
             new_comment.save()
-            return redirect("releases:single_release_page" , slug = slug)
+            json_comment = model_to_dict(new_comment)
+            return JsonResponse({"has_content" : True , "comment" : json_comment , "is_comment_user_admin" : new_comment.related_user.is_staff , "comment_user_name" : new_comment.related_profile().full_name , "comment_time" : new_comment.get_time() , "comment_date" : new_comment.get_solar_date() , "status" : "success" , "message" : "Data saved successfully" , "allowed_to_del" : request.user == new_comment.related_user or request.user.is_staff})
         else  :
             article = Article.objects.get(slug = slug)
             new_comment = Comment(content = comment_content , related_article = article , related_user = request.user)
             if new_comment.content == "" :       
-                return redirect("news:single_article_page" , slug = new_comment.related_article.slug)
+                return JsonResponse({"has_content" : False , "status" : "success" , "message" : "Data saved successfully"})
             new_comment.save()
-            return redirect("news:single_article_page" , slug = slug)
+            json_comment = model_to_dict(new_comment)
+            return JsonResponse({"has_content" : True , "comment" : json_comment , "is_comment_user_admin" : new_comment.related_user.is_staff , "comment_user_name" : new_comment.related_profile().full_name , "comment_time" : new_comment.get_time() , "comment_date" : new_comment.get_solar_date() , "status" : "success" , "message" : "Data saved successfully" , "allowed_to_del" : request.user == new_comment.related_user or request.user.is_staff})
 def delete_comment(request , id):
     comment = Comment.objects.get(id = id)
     if request.user == comment.related_user or request.user.is_staff : 
         if comment.related_release :
             comment.delete()
-            return redirect("releases:single_release_page" , slug = comment.related_release.slug)
+            data = {"status" : "success" , "message" : "Data saved successfully"}
+            return JsonResponse(data)
         else:
             comment.delete()
-            return redirect("news:single_article_page" , slug = comment.related_article.slug)
+            data = {"status" : "success" , "message" : "Data saved successfully"}
+            return JsonResponse(data)
     else:
         if comment.related_release :
             return redirect("releases:single_release_page" , slug = comment.related_release.slug)
--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,3 +1,126 @@
+const commentsForm = document.querySelector('.comments__form')
+commentsForm.addEventListener('submit' ,
+    function(event){
+        event.preventDefault() ;
+        const input = document.querySelector('textarea') ;
+        const commentContent = input.value ;
+        const slug = commentsForm.getAttribute('data-slug') ;
+        const baseUrl = window.location.origin ;
+        const url = `${baseUrl}/comments/adding-comment/${slug}`;   
+        fetch(url, {
+            method: 'POST' ,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken')
+            } ,
+            body : JSON.stringify({content : commentContent})
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.status === 'success') {
+                if (data.has_content){
+                    const ul = document.getElementById('comments_list_') ;
+                    const newLi = document.createElement('li') ;
+                    newLi.setAttribute('class' , 'comments__item') ;
+                    if (data.is_comment_user_admin){
+                    newLi.innerHTML = `
+                        <div class="comments__autor">
+                                        <img class="comments__avatar" src="/static/img/admin.jpg" alt="">
+                                        <span style = "color: #B3FF80; "class="comments__name"><b>${data.comment_user_name}</b></span>
+                                        <span class="comments__time">${data.comment_time} , ${data.comment_date}</span>
+                                    </div>
+                                    <p class="comments__text">${data.comment.content}</p>
+                                    <div class="comments__actions">
+                                        <div class="comments__rate">
+    
+                                        </div>
+                                        <button style="color: #c0c0c0 ; margin-left: 15px;" onclick="delComment(${data.comment.id} , this)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
+                                            <path d="M14 3a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2M3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5s-3.69-.311-4.785-.793"/>
+                                          </svg>
+                                        </butoon>
+                                    </div>
+                    `  } else {
+                        newLi.innerHTML = `
+                                            <div class="comments__autor">      
+                                        <img class="comments__avatar" src="/static/img/avatar.svg" alt="">
+                                        <span class="comments__name">${data.comment_user_name}</span>
+                                        <span class="comments__time">${data.comment_time} , ${data.comment_date}</span>
+                                    </div>
+                                    <p class="comments__text">${data.comment.content}</p>
+                                    <div class="comments__actions">
+                                        <div class="comments__rate">
+    
+                                        </div>
+                                    <button style="color: #c0c0c0 ; margin-left: 15px;" onclick="delComment(${data.comment.id} , this)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash2" viewBox="0 0 16 16">
+                                            <path d="M14 3a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2M3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5s-3.69-.311-4.785-.793"/>
+                                          </svg>
+                                        </butoon>
+                                    </div>
+                        ` 
+                    };
+                    ul.insertBefore(newLi , ul.firstChild) ;
+                    input.value = '' ;
+                    len_comments = document.getElementById('len_comments') ;
+                    len_comments.innerHTML = Number(len_comments.innerHTML) + 1 ; 
+                    const notifcation = document.getElementById('notification2') ;
+                    notifcation.classList.add('show') ;
+                    setTimeout(() => {
+                    notifcation.classList.remove('show') ;
+                    } , 8000 ) ;                 
+                } ;
+            } else {
+                console.error('Error saving data:', data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+    
+) ;
+function delComment(id , button) {
+    const baseUrl = window.location.origin ;
+    const url = `${baseUrl}/comments/delete-comment/${id}`;
+    fetch(url, {
+        method: 'GET' ,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.status === 'success') {
+            const li = button.closest('li') ;
+            li.classList.replace('comments__item' , 'comments__form') ;
+            li.remove() ;
+            const notifcation = document.getElementById('notification1') ;
+            notifcation.classList.add('show') ;
+            setTimeout(() => {
+                notifcation.classList.remove('show') ;
+            } , 8000 ) ;
+            len_comments = document.getElementById('len_comments') ;
+            len_comments.innerHTML = Number(len_comments.innerHTML) - 1 ; 
+        } else {
+            console.error('Error saving data:', data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+} ;
 function addToFav(slug) {
     const baseUrl = window.location.origin ;
     const url = `${baseUrl}/favorites/add-to-favorites/${slug}`;
@@ -17,7 +140,6 @@ function addToFav(slug) {
     })
     .then(data => {
         if (data.status === 'success') {
-            console.log('Data saved successfully:', data.message);
             addToFavContainer.innerHTML = `
                   <button style="width: 40px; height: 40px;" onclick="delFromFav('${slug}')" class="single-item__add">
                             <svg style="width: 23px; height: 23px;" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-file-earmark-x" viewBox="0 0 16 16">
@@ -34,7 +156,7 @@ function addToFav(slug) {
     .catch(error => {
         console.error('Error:', error);
     });
-}
+} ;
 function delFromFav(slug) {
     const baseUrl = window.location.origin ;
     const url = `${baseUrl}/favorites/delete-from-favorites/${slug}`;
@@ -54,7 +176,6 @@ function delFromFav(slug) {
     })
     .then(data => {
         if (data.status === 'success') {
-            console.log('Data saved successfully:', data.message);
             addToFavContainer.innerHTML = `
                 <button id="addToFav" style="width: 40px; height: 40px;" onclick="addToFav('${slug}')" class="single-item__add">
                 <svg style="width: 25px; height: 25px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,11H13V5a1,1,0,0,0-2,0v6H5a1,1,0,0,0,0,2h6v6a1,1,0,0,0,2,0V13h6a1,1,0,0,0,0-2Z"/></svg>
@@ -83,4 +204,4 @@ function getCookie(name) {
         }
     }
     return cookieValue;
-}
+} ;
